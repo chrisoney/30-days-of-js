9fd9a63fb2a29a0ea57ffaa6fc70d54df9d6a420
chrisoney <christopher.oney@yahoo.com>
2020-10-02 19:43:00 -0700

    adding days hash

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let name;
+	name = "chris"
 </script>
 
 <main>
---
+++ src/days.js
@@ -0,0 +1,152 @@
+const days = {
+  1: {
+    title: "01 - Javascript Drum Kit",
+    completed: false,
+    file: "<DayOne />"
+  },
+  2: {
+    title: "02 - JS and CSS Clock",
+    completed: false,
+    file: "<DayTwo />"
+  },
+  3: {
+    title: "03 - CSS Variables",
+    completed: false,
+    file: "<DayThree />"
+  },
+  4: {
+    title: "04 - Array Cardio Day 1",
+    completed: false,
+    file: "<DayFour />"
+  },
+  5: {
+    title: "05 - Flex Panel Gallery",
+    completed: false,
+    file: "<DayFive />"
+  },
+  6: {
+    title: "06 - Type Ahead",
+    completed: false,
+    file: "<DaySix />"
+  },
+  7: {
+    title: "07 - Array Cardio Day 2",
+    completed: false,
+    file: "<DaySeven />"
+  },
+  8: {
+    title: "08 - Fun with HTML5 Canvas",
+    completed: false,
+    file: "<DayEight />"
+  },
+  9: {
+    title: "09 - Dev Tools Domination",
+    completed: false,
+    file: "<DayNine />"
+  },
+  10: {
+    title: "10 - Hold Shift and Check Checkboxes",
+    completed: false,
+    file: "<DayTen />"
+  },
+  11: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayEleven />"
+  },
+  12: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwelve />"
+  },
+  13: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayThirteen />"
+  },
+  14: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayFourteen />"
+  },
+  15: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayFifteen />"
+  },
+  16: {
+    title: "Javascript",
+    completed: false,
+    file: "<DaySixteen />"
+  },
+  17: {
+    title: "Javascript",
+    completed: false,
+    file: "<DaySeventeen />"
+  },
+  18: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayEighteen />"
+  },
+  19: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayNineteen />"
+  },
+  20: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwenty />"
+  },
+  21: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyOne />"
+  },
+  22: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyTwo />"
+  },
+  23: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyThree />"
+  },
+  24: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyFour />"
+  },
+  25: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyFive />"
+  },
+  26: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentySix />"
+  },
+  27: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentySeven />"
+  },
+  28: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyEight />"
+  },
+  29: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayTwentyNine />"
+  },
+  30: {
+    title: "Javascript",
+    completed: false,
+    file: "<DayThirty />"
+  },
+}
