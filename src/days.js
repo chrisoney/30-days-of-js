79c4885e21de92c26fe8d07d44191f3e714fbc51
chrisoney <christopher.oney@yahoo.com>
2020-10-20 08:52:14 -0700

    fix days object

--- src/days.js
+++ src/days.js
@@ -4,158 +4,156 @@ const days = {
     day: 'home',
     title: "Home",
     completed: true,
-    file: './day_files/Home.svelte'
   },
   1: {
     day: 1,
     title: "01 - Javascript Drum Kit",
-    completed: false,
-    file: "<DayOne />"
+    completed: true,
   },
   2: {
+    day: 2, 
     title: "02 - JS and CSS Clock",
     completed: false,
-    file: "<DayTwo />"
   },
   3: {
+    day: 3,
     title: "03 - CSS Variables",
     completed: false,
-    file: "<DayThree />"
   },
   4: {
+    day: 4,
     title: "04 - Array Cardio Day 1",
     completed: false,
-    file: "<DayFour />"
   },
   5: {
+    day: 5,
     title: "05 - Flex Panel Gallery",
     completed: false,
-    file: "<DayFive />"
   },
   6: {
+    day: 6,
     title: "06 - Type Ahead",
     completed: false,
-    file: "<DaySix />"
   },
   7: {
+    day: 7,
     title: "07 - Array Cardio Day 2",
     completed: false,
-    file: "<DaySeven />"
   },
   8: {
+    day: 8,
     title: "08 - Fun with HTML5 Canvas",
     completed: false,
-    file: "<DayEight />"
   },
   9: {
+    day: 9,
     title: "09 - Dev Tools Domination",
     completed: false,
-    file: "<DayNine />"
   },
   10: {
+    day: 10,
     title: "10 - Hold Shift and Check Checkboxes",
     completed: false,
-    file: "<DayTen />"
   },
   11: {
+    day: 11,
     title: "11 - Custom Video Player",
     completed: false,
-    file: "<DayEleven />"
   },
   12: {
+    day: 12,
     title: "12 - Key Sequence Detection",
     completed: false,
-    file: "<DayTwelve />"
   },
   13: {
+    day: 13,
     title: "13 - Slide in on Scroll",
     completed: false,
-    file: "<DayThirteen />"
   },
   14: {
+    day: 14,
     title: "14 - JavaScript References VS Copying",
     completed: false,
-    file: "<DayFourteen />"
   },
   15: {
+    day: 15,
     title: "15 - LocalStorage",
     completed: false,
-    file: "<DayFifteen />"
   },
   16: {
+    day: 16,
     title: "16 - Mouse Move Shadow",
     completed: false,
-    file: "<DaySixteen />"
   },
   17: {
+    day: 17,
     title: "17 - Sort Without Articles",
     completed: false,
-    file: "<DaySeventeen />"
   },
   18: {
+    day: 18,
     title: "18 - Adding Up Times with Reduce",
     completed: false,
-    file: "<DayEighteen />"
   },
   19: {
+    day: 19,
     title: "19 - Webcam Fun",
     completed: false,
-    file: "<DayNineteen />"
   },
   20: {
+    day: 20,
     title: "20 - Speech Detection",
     completed: false,
-    file: "<DayTwenty />"
   },
   21: {
+    day: 21,
     title: "21 - Geolocation",
     completed: false,
-    file: "<DayTwentyOne />"
   },
   22: {
+    day: 22,
     title: "22 - Follow Along Link Highlighter",
     completed: false,
-    file: "<DayTwentyTwo />"
   },
   23: {
+    day: 23,
     title: "23 - Speech Synthesis",
     completed: false,
-    file: "<DayTwentyThree />"
   },
   24: {
+    day: 24,
     title: "24 - Sticky Nav",
     completed: false,
-    file: "<DayTwentyFour />"
   },
   25: {
+    day: 25,
     title: "25 - Event Capture, Propagation, Bubbling and Once",
     completed: false,
-    file: "<DayTwentyFive />"
   },
   26: {
+    day: 26,
     title: "26 - Stripe Follow Along Nav",
     completed: false,
-    file: "<DayTwentySix />"
   },
   27: {
+    day: 27,
     title: "27 - Click and Drag",
     completed: false,
-    file: "<DayTwentySeven />"
   },
   28: {
+    day: 28,
     title: "28 - Video Speed Controller",
     completed: false,
-    file: "<DayTwentyEight />"
   },
   29: {
+    day: 29,
     title: "29 - Countdown Timer",
     completed: false,
-    file: "<DayTwentyNine />"
   },
   30: {
+    day: 30,
     title: "30 - Whack A Mole",
-    completed: false,
-    file: "<DayThirty />"
+    completed: true,
   },
 }
 
