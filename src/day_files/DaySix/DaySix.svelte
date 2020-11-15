7b39ffd3a2c7ff520059a18ca1e59618bdc1e9a6
chrisoney <christopher.oney@yahoo.com>
2020-11-14 23:10:08 -0800

    fill in starter info for day 6

--- src/day_files/DaySix/DaySix.svelte
+++ src/day_files/DaySix/DaySix.svelte
@@ -1,9 +1,17 @@
 <script>
+	const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
 
 </script>
 
 <div class="day-main">
-  Day Six
+  <form class="search-form">
+    <input type="text" class="search" placeholder="City or State">
+    <ul class="suggestions">
+      <li>Filter for a city</li>
+      <li>or a state</li>
+    </ul>
+  </form>
 </div>
 
 <style>
