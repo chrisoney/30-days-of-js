5bfeee13db9d46f2a29debdc5e4dd72833ad9b9c
chrisoney <christopher.oney@yahoo.com>
2020-11-15 21:10:05 -0800

    add more details to day 6

--- src/day_files/DaySix/DaySix.svelte
+++ src/day_files/DaySix/DaySix.svelte
@@ -1,7 +1,21 @@
 <script>
 	const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+	const cities = [];
+	fetch(endpoint)
+		.then(blob => blob.json())
+		.then(data => cities.push(...data))
 
 
+	function findMatches(word, cities){
+		return cities.filter(place => {
+			const regex = new RegExp(word, 'gi');
+			return place.city.match(regex) || place.state.match(regex);
+		})
+	}
+
+	function displayMatches(){
+
+	}
 </script>
 
 <div class="day-main">
